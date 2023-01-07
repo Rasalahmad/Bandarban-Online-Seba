45c66ce05650ad40913ed7682c00a1744cdbf953
Rasalahmad <rasalahmadsit@gmail.com>
2023-01-07 19:33:29 +0600

    wip

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Rang!! Your app development is started</Text>
+      <Text>Hello Rang!!</Text>
       <StatusBar style="auto" />
     </View>
   );
