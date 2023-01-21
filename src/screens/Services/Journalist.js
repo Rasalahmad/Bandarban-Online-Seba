e191ce810aac1f74b1a1175ac24ab12d99c813fd
Rasalahmad <rasalahmadsit@gmail.com>
2023-01-21 23:26:20 +0600

    wip

---
+++ src/screens/Services/Journalist.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Journalist() {
+  return (
+    <View>
+      <Text>Journalist</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
